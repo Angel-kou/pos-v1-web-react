beb422c6d76fa550d11103ba34074d61d2c7c33b
mjkou <1844646307@qq.com>
2018-07-29 15:59:57 +0800

    complete cart fun

--- src/App.js
+++ src/App.js
@@ -1,11 +1,130 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import './App.css';
+import { loadAllItems, loadPromotions } from './database';
+import ListItem from '../src/ListItem.js';
+import CartItem from '../src/CartItem.js';
+import PromotionItem from '../src/PromotionItem.js';
 
-class App extends Component {
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      AllItems: this.getAllItems(),
+      cartPage: false,
+      cart: []
+    };
+  }
+
+  getAllItems = () => {
+    const allItems = loadAllItems().map(item => {
+      item['cartCount'] = 0;
+      item['isDiscount'] = false;
+      return item;
+    });
+
+    allItems.map(item => {
+      if (loadPromotions()[0].barcodes.includes(item.barcode)) {
+        item['isDiscount'] = true;
+      } else {
+        item['isDiscount'] = false;
+      }
+      return item;
+    });
+    console.log('items--kmj 9999 ', allItems);
+    return allItems;
+  };
+
+  togglePage = () => {
+    this.setState({
+      cart: !this.state.cartPage ? this.state.cart : [],
+      cartPage: !this.state.cartPage
+    });
+  };
+
+  addItemToCart = (addedItem, count) => {
+    if (count != 0) {
+      const item = this.state.cart.find(
+        item => item.barcode === addedItem.barcode
+      );
+      console.log('find', item);
+      if (item !== undefined) {
+        item.cartCount += count;
+      } else {
+        addedItem.cartCount += count;
+        this.state.cart.push(addedItem);
+      }
+
+      this.setState({
+        cart: this.state.cart
+      });
+    }
+    console.log('cart', this.state.cart);
+  };
+
+  confirmBuy = () => {
+    const result = this.state.cart.map(
+      item => item.barcode + '-' + item.cartCount
+    );
+    console.log(result);
+    alert(result);
+  };
+
   render() {
     return (
       <div className="App">
-       app
+        {!this.state.cartPage ? (
+          <div>
+            <h1>ITEM List</h1>
+            <div className="AllItems">
+              {this.state.AllItems.map(item => (
+                <ListItem
+                  key={item.barcode}
+                  item={item}
+                  isCartPage={this.state.cartPage}
+                  itemToCart={this.addItemToCart}
+                />
+              ))}
+            </div>
+
+            <button
+              type="button"
+              className="btn btn-primary"
+              id="cart"
+              onClick={this.togglePage}
+            >
+              购物车
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h1>Cart List</h1>
+            <div className="AllItems">
+              {this.state.cart.map(item => (
+                <CartItem
+                  key={item.barcode}
+                  item={item}
+                  isCartPage={this.state.cartPage}
+                />
+              ))}
+            </div>
+            <button
+              type="button"
+              className="btn btn-primary"
+              id="cart"
+              onClick={this.confirmBuy}
+            >
+              确认
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              id="cart"
+              onClick={this.togglePage}
+            >
+              返回
+            </button>
+          </div>
+        )}
       </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -3,18 +3,36 @@ import React, { PureComponent } from 'react';
 class CartItem extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      price: 0
+    };
+  }
+
+  componentWillMount() {
+    console.log('kmj ---- mount');
+    const item = this.props.item;
+    const price =
+      item.isDiscount && item.cartCount > 2
+        ? (item.cartCount - 1) * item.price
+        : item.cartCount * item.price;
+    this.setState({
+      price: price.toFixed(2)
+    });
   }
 
   render() {
     const item = this.props.item;
+
     return (
-      <div key={item.barcode} className="ListItem">
-        <span className="itemName">{item.name}</span>
-        <span className="price">
-          {item.price}元/{item.unit}
-        </span>
-        <span>数量：{item.cartCount}</span>
+      <div>
+        <div key={item.barcode} className="ListItem">
+          <span className="itemName">{item.name}</span>
+          <span className="price">
+            {item.price}元/{item.unit}
+          </span>
+          <span>数量：{item.cartCount}</span>
+          <span>小计：{this.state.price}元</span>
+        </div>
       </div>
     );
   }
